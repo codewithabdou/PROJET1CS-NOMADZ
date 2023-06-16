82a86024d268d3bef5ca450d2235fe59d35c9705
Zaki-h2 <105202293+Zaki-h2@users.noreply.github.com>
2023-06-16 23:38:48 +0100

    another favoris fix

--- app/api/favoris/route.js
+++ app/api/favoris/route.js
@@ -9,12 +9,10 @@ import { authOptions } from "../auth/[...nextauth]/route"
 
 
 export async function POST(request){
-   // const session = await getServerSession(authOptions)
-    // if(!session)
-    //     return NextResponse.json({message:"Unauthorized : you must be signed in"}, {status: 401})
+   const session = await getServerSession(authOptions)
+    if(!session)
+        return NextResponse.json({message:"Unauthorized : you must be signed in"}, {status: 401})
 
-    const session = {user:{email:"kz_hamdane@esi.dz"}}
-    
     try{
         const email = session.user.email
         const user = await prisma.user.findUnique({where:{email}})      
@@ -32,11 +30,9 @@ export async function POST(request){
 
 export async function GET(){
 
-    // const session = await getServerSession(authOptions)
-    // if(!session)
-    //     return NextResponse.json({message:"Unauthorized : you must be signed in"}, {status: 401})
-
-    const session = {user:{email:"kz_hamdane@esi.dz"}}
+    const session = await getServerSession(authOptions)
+    if(!session)
+        return NextResponse.json({message:"Unauthorized : you must be signed in"}, {status: 401})
 
     try{
         const email = session.user.email
